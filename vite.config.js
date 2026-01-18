137258234a81e66ad6001ef099f9c75a75b5fa19
Minh Pham <pdminh180999@gmail.com>
2026-01-18 23:33:40 +0700

    for deployment

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [react()],
+    base: "/assessment/",
     build: {
         outDir: 'dist',
-        lib: {
-            entry: 'src/main.jsx',
-            name: 'WishlistWidget',
-            fileName: 'wishlist-widget',
-            formats: ['es', 'umd']
-        },
         rollupOptions: {
             external: ['react', 'react-dom'],
             output: {
